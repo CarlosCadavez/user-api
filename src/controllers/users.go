2e0f535c8ca4961055b7a463a9bedb14553e4a51
CarlosCadavez <cs.cadavez@gmail.com>
2021-11-06 08:24:44 -0300

    feat: user validation

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -23,6 +23,12 @@ func CreateUser(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	//Validating user
+	if err = user.Prepare(); err != nil {
+		responses.Err(w, http.StatusBadRequest, err)
+		return
+	}
+
 	db, err := database.Connect()
 	if err != nil {
 		responses.Err(w, http.StatusInternalServerError, err)
