346f2f62340b205f7c374504be80d0724eb74733
CarlosCadavez <cs.cadavez@gmail.com>
2021-11-06 05:52:42 -0300

    feat: save user

--- src/controllers/users.go
+++ src/controllers/users.go
@@ -1,9 +1,40 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"user-api/src/database"
+	"user-api/src/models"
+	"user-api/src/repositories"
+)
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Shoud create a new user"))
+	bodyRequest, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var user models.User
+	if err = json.Unmarshal(bodyRequest, &user); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := database.Connect()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	repository := repositories.NewUserRepository(db)
+	userId, err := repository.Create(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	w.Write([]byte(fmt.Sprintf("Id inserted: %d", userId)))
+
 }
 
 func GetAllUsers(w http.ResponseWriter, r *http.Request) {
--- src/database/database.go
+++ src/database/database.go
@@ -2,18 +2,23 @@ package database
 
 import (
 	"database/sql"
+	"fmt"
 	"user-api/src/config"
 
 	_ "github.com/go-sql-driver/mysql" // Driver
 )
 
 func Connect() (*sql.DB, error) {
+	fmt.Print(config.StringConnectionDatabase)
 	db, err := sql.Open("mysql", config.StringConnectionDatabase)
 	if err != nil {
+		fmt.Print("Erro open")
 		return nil, err
 	}
 
 	if err = db.Ping(); err != nil {
+		fmt.Print("Erro ping")
+
 		db.Close()
 		return nil, err
 	}
