3eeea3befe407de5faeb9f60caf4957a53ab2bcf
CarlosCadavez <cs.cadavez@gmail.com>
2021-12-11 13:23:18 -0300

    feat: pass encrypted

--- src/models/User.go
+++ src/models/User.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"strings"
 	"time"
+	"user-api/src/security"
 
 	"github.com/badoux/checkmail"
 )
@@ -41,10 +42,21 @@ func (user *User) validate(step string) error {
 	return nil
 }
 
-func (user *User) format() {
+func (user *User) format(step string) error {
 	user.Name = strings.TrimSpace(user.Name)
 	user.Nick = strings.TrimSpace(user.Nick)
 	user.Email = strings.TrimSpace(user.Email)
+
+	if step == "create" {
+		hashedPassword, err := security.Hash(user.Password)
+		if err != nil {
+			return err
+		}
+
+		user.Password = string(hashedPassword)
+	}
+
+	return nil
 }
 
 func (user *User) Prepare(step string) error {
@@ -52,6 +64,8 @@ func (user *User) Prepare(step string) error {
 		return err
 	}
 
-	user.format()
+	if err := user.format(step); err != nil {
+		return err
+	}
 	return nil
 }
